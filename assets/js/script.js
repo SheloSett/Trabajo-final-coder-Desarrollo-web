d3eced9942a7108a87e2aed1af2ed5190e6573e3
Shelo Setton <shelosetton@gmail.com>
2022-12-08 21:52:02 -0300

    final

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+function calculoDePrecio1(){
+    var precios = document.getElementById("cantidad-precio1").value;
+    document.getElementById("precio-calculado1").innerHTML = precios + "$"
+}
+
+function calculoDePrecio2(){
+    var precios = document.getElementById("cantidad-precio2").value;
+    document.getElementById("precio-calculado2").innerHTML = precios + "$"
+}
+
+function calculoDePrecio3(){
+    var precios = document.getElementById("cantidad-precio3").value;
+    document.getElementById("precio-calculado3").innerHTML = precios + "$"
+}
+
+function calculoDePrecio4(){
+    var precios = document.getElementById("cantidad-precio4").value;
+    document.getElementById("precio-calculado4").innerHTML = precios + "$"
+}
+
+function calculoDePrecio5(){
+    var precios = document.getElementById("cantidad-precio5").value;
+    document.getElementById("precio-calculado5").innerHTML = precios + "$"
+}
+
+function calculoDePrecio6(){
+    var precios = document.getElementById("cantidad-precio6").value;
+    document.getElementById("precio-calculado6").innerHTML = precios + "$"
+}
+
+function calculoDePrecio7(){
+    var precios = document.getElementById("cantidad-precio7").value;
+    document.getElementById("precio-calculado7").innerHTML = precios + "$"
+}
+
+function calculoDePrecio8(){
+    var precios = document.getElementById("cantidad-precio8").value;
+    document.getElementById("precio-calculado8").innerHTML = precios + "$"
+}
